f98b4b21de5d0f7cfa22445880c17486ffe8e0c0
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-10-21 11:36:59 -0400

    backend start

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const app = express();
 
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@email.com",
+      password: "12345",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "sally",
+      email: "sally@email.com",
+      password: "1234",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
   res.send("this is working");
+});
+
+app.post("/signin", (req, res) => {
+  res.json("this is sign in");
 });
 
 app.listen(3000, () => {
