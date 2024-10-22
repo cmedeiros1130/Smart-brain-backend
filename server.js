1180927c7fa57b6a1cb3f1e273b6311a1dcff970
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-10-22 12:44:27 -0400

    cleaned up

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const knex = require("knex");
 
 const db = knex({
-  // Enter your own database information here based on what you created
   client: "pg",
   connection: {
     host: "127.0.0.1",
@@ -18,12 +17,7 @@ const db = knex({
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
-
-// Test only - when you have a database variable you want to use
-// app.get('/', (req, res)=> {
-//   res.send(database.users);
-// })
+app.use(express.json());
 
 app.post("/signin", (req, res) => {
   db.select("email", "hash")
@@ -62,10 +56,6 @@ app.post("/register", (req, res) => {
         return trx("users")
           .returning("*")
           .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
             email: loginEmail[0].email,
             name: name,
             joined: new Date(),
@@ -101,10 +91,6 @@ app.put("/image", (req, res) => {
     .increment("entries", 1)
     .returning("entries")
     .then((entries) => {
-      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-      // entries[0] --> this used to return the entries
-      // TO
-      // entries[0].entries --> this now returns the entries
       res.json(entries[0].entries);
     })
     .catch((err) => res.status(400).json("unable to get entries"));
